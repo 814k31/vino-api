a3564283448b550f52cb5fda9cd16ba01ebf51c1
Blake Lovelace <blovelace@elementengineering.com.au>
2020-11-16 22:06:47 +0800

    Update Batch to have DateCreated and DateUpdated

--- vino-api/Migrations/20201116134916_AddBatchTimeStamps.Designer.cs
+++ vino-api/Migrations/20201116134916_AddBatchTimeStamps.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using vino_api.Models;
 
 namespace vino_api.Migrations
 {
     [DbContext(typeof(BatchContext))]
-    partial class BatchContextModelSnapshot : ModelSnapshot
+    [Migration("20201116134916_AddBatchTimeStamps")]
+    partial class AddBatchTimeStamps
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
